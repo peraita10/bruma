cd1cab7af4035fa61c5e6d12a994b57e1f0f8b92
jaime <jaime.peraita@hubexo.com>
2026-08-24 22:22:30 +0200

    .

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,38 @@
-const CACHE = 'bruma-v1';
-const ASSETS = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+const CACHE = 'bruma-v2';
+
+const BASE = '/bruma/';
+
+const ASSETS = [
+  BASE,
+  BASE + 'manifest.webmanifest',
+  BASE + 'icon-192.png',
+  BASE + 'icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(hit => hit || fetch(event.request))
+  );
+});
