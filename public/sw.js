04690f26fe13cdec24d22bc01deb69233a02793f
peraita10 <124808143+peraita10@users.noreply.github.com>
2026-08-24 22:53:59 +0200

    Fix stale PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,38 +1,17 @@
-const CACHE = 'bruma-v2';
+const CACHE = 'bruma-v3';
 
-const BASE = '/bruma/';
-
-const ASSETS = [
-  BASE,
-  BASE + 'manifest.webmanifest',
-  BASE + 'icon-192.png',
-  BASE + 'icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    )
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(hit => hit || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
