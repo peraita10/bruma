e78f641ad43c96aa32ecb9781b0a83ff3624b527
peraita10 <124808143+peraita10@users.noreply.github.com>
2026-08-24 23:00:55 +0200

    Use stable asset names for Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/bruma/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/app.js",
+        chunkFileNames: "assets/chunk-[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
 });
