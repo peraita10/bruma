4928ea4dabb48f13c38d550fab4c779112615c43
peraita10 <124808143+peraita10@users.noreply.github.com>
2026-09-01 13:46:09 +0200

    Initialize Umami analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import App from './App';
+import { initAnalytics } from './analytics';
+
+initAnalytics();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
